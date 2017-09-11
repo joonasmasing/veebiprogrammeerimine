ded4abab93a22533593f92eb2550461eab087f90
Joonas Masing <joonasmasing@users.noreply.github.com>
2017-09-11 13:48:46 +0300

    Esimene php fail, kus on vaid HTML

---
+++ esimene.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Joonas Masing staff</title>
+</head>
+<body>
+<h1>Joonas Masing</h1>
+<p>See veebileht on loodud õppetöö raames ning ei sisalda tõsiseltvõetavat sisu.</p>
+<p>This website is made for educational purposes only.</p>
+</body>
+</html>
