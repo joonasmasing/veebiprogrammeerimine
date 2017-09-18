81b12e68a2eabd57c27054a0eaf5d4a5e7f0e88d
Joonas Masing <joonasmasing@users.noreply.github.com>
2017-09-18 15:30:04 +0300

    Palju asju, olen tubli ja õpin uusi asju ju

---
+++ tund3/foto.php
@@ -0,0 +1,21 @@
+<?php
+$myName = "Joonas";
+$myFamilyName = "Masing";
+$hourNow = date("H");
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Joonas Masing - Pilt ülikoolist</title>
+</head>
+<body>
+<p>See veebileht on loodud õppetöö raames ning ei sisalda tõsiseltvõetavat sisu.</p>
+<h2>Pilte ülikoolist </h2>
+<img src="../../pics/tlu_29.jpg" alt="Tallinna Ülikool">
+<img src="../../pics/tlu_42.jpg" alt="Tallinna Ülikool">
+<img src="../../pics/tlu_15.jpg" alt="Tallinna Ülikool">
+<img src="../../pics/tlu_11.jpg" alt="Tallinna Ülikool">
+</body>
+</html>
